36219cef2f6ef1b3040facfb2cee5ad66492dfdd
hari0082 <2430680@rts-365.dk>
2024-01-09 13:01:26 +0100

    Add headertop and new pages

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
 import "./index.css";
+import ControlPanel from "./pages/controlpanel";
 
 function App() {
   return (
     <>
-      <div>Hello</div>
+      <div className="flex justify-between p-6 items-center ">
+        <h1 className="text-white text-3xl font-bold">
+          Control <br /> Panel
+        </h1>
+        <span className="rounded-full border-2 solid border-gray-300 size-14">
+          <img
+            className="rounded-full"
+            src="https://xsgames.co/randomusers/avatar.php?g=pixel"
+            alt=""
+          />
+        </span>
+      </div>
+      <ControlPanel />
     </>
   );
 }
---
+++ my-react-app/src/pages/Controlpanel.jsx
@@ -0,0 +1,9 @@
+const ControlPanel = () => {
+  return (
+    <>
+      <div className="bg-white"></div>
+    </>
+  );
+};
+
+export default ControlPanel;
